3ec9157616ca6afb896ffe3cdd15d3e905de9347
developer <boris-plus@mail.ru>
2018-09-01 09:17:21 +0300

    приложение с демонстрацией возможностей dummy

--- examples_usage/app_features_demonstration/controllers/index.py
+++ examples_usage/app_features_demonstration/controllers/index.py
@@ -1,13 +1,3 @@
-#!/usr/bin/python3
-import sys
-import os
-
-
-dummy_wsgi_framework_module_path = os.path.dirname(
-    os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
-if dummy_wsgi_framework_module_path not in sys.path:
-    sys.path.append(dummy_wsgi_framework_module_path)
-
 
 def controller_response(environ, start_response, app_config):
     if environ:
@@ -18,10 +8,23 @@ def controller_response(environ, start_response, app_config):
             "<b>Привет. Я стартовая страница приложения \"%s\".</b> <br>"
             "Посмотри: <br>"
             " * <a href='./'>'Стартовая страница'</a> - то, что сейчас видите<br>"
+            ""
             " * <a href='./homework_rebuke'>'Типовые ошибки ДЗ'</a> - контроллер без представления<br>"
-            " * <a href='./string_methods' target='_blank'>'Строковые методы'</a> - контроллер с HTML-представления<br>"
-            " * <a href='./string_functions'>'Строковые функции'</a> - HTML-представление умышленно отсутствует<br>"
-            " * <a href='./a_priori_bad_way'>Нет пути</a> - в Routes соответствия данному URI<br>"
+            ""
+            " * <a href='./string_methods' target='_blank'>'Строковые методы (новое окно)'</a> - "
+            " контроллер с HTML-представлением<br>"
+            ""
+            " * <a href='./a_priori_bad_way'>'Нет маршрута'</a> - в маршрутах приложения нет соответствия данному URI"  
+            " (<i><b>проверка RouteDoesNotExists</b></i>)<br>"    
+            ""
+            " * <a href='./controller_file_does_not_exists'>'Нет файла контроллера'</a> - путь есть, "
+            " а файл контроллера умышленно отсутствует</a>"  
+            " (<i><b>проверка ControllerFileDoesNotExists</b></i>)<br>"    
+            ""
+            " * <a href='./controller_with_missing_view_file'>'Нет файла представления'</a> - маршрут есть,"
+            " контроллер есть, а HTML-файл представления умышленно отсутствует"
+            " (<i><b>проверка ViewDoesNotExists</b></i>)<br>"
+            ""      
             "" % app_config.APP_NAME
             , 'utf-8'
         )
--- examples_usage/app_features_demonstration/routes.py
+++ examples_usage/app_features_demonstration/routes.py
@@ -12,7 +12,8 @@ uri_routes = base_uri_routes.copy()
 uri_routes.update(
     **{
         '/homework_rebuke': 'homework_rebuke.py',
-        '/string_functions': 'string_functions.py',
         '/string_methods': 'string_methods.py',
+        '/controller_file_does_not_exists': 'controller_file_does_not_exists.py',
+        '/controller_with_missing_view_file': 'controller_with_missing_view_file.py',
     }
 )
