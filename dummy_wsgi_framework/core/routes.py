8e1062f73e0b4a42577c66b7ea1e1c4914f73191
developer <boris-plus@mail.ru>
2018-09-01 07:25:09 +0300

    логика поиска контроллера по URI из маршрутов теперь опирается на config приложения

---
+++ dummy_wsgi_framework/core/routes.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+from dummy_wsgi_framework.core.exceptions import ControllerFileDoesNotExists, RouteDoesNotExists
+import sys
+import os
+
+DEFAULT = 'default'
+ERROR_404 = 'error_404'
+base_uri_routes = {
+    '': 'index.py',
+    DEFAULT: 'index.py',
+    ERROR_404: 'error_404.py',
+}
+
+
+def get_controller_by_uri(request_uri, app_config):
+    try:
+        if app_config.APP_ROOT_DIR not in sys.path:
+            sys.path.insert(0, app_config.APP_ROOT_DIR)
+        if os.path.exists(os.path.join(app_config.APP_ROOT_DIR, 'routes.py')):
+            routes_module = __import__("routes")
+            uri_routes = routes_module.uri_routes
+        else:
+            uri_routes = base_uri_routes
+    except ImportError:
+        raise
+    controller = uri_routes.get(
+        request_uri, uri_routes.get(
+            "%s" % request_uri.rstrip('/'),
+            None
+        )
+    )
+    if controller is None:
+        raise RouteDoesNotExists
+    if not os.path.exists(os.path.join(app_config.APP_CONTROLLERS_DIR, controller)):
+        raise ControllerFileDoesNotExists
+    return controller
