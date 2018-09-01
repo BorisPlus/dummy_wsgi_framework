8e8938efd7ca07eaa4f9a2e644d46d8a23806af7
developer <boris-plus@mail.ru>
2018-09-01 07:23:23 +0300

    Определил базовые исключительные ситуации

---
+++ dummy_wsgi_framework/core/exceptions.py
@@ -0,0 +1,14 @@
+class ControllerFileDoesNotExists(Exception):
+    pass
+
+
+class RouteDoesNotExists(Exception):
+    pass
+
+
+class ViewDoesNotExists(Exception):
+    pass
+
+
+class BadTermUsage(Exception):
+    pass
