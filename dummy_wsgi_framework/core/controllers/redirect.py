9aa8f5080c992f0f3688e1248e2bd40165f60916
developer <boris-plus@mail.ru>
2018-09-12 02:17:27 +0300

    заменил название метода controller_response на get_response

--- dummy_wsgi_framework/core/controllers/redirect.py
+++ dummy_wsgi_framework/core/controllers/redirect.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-def controller_response(environ, start_response, app_config, location):
-    if environ:
-        pass  # Lets ignore not usage PyCharm
-    if app_config:
+def get_response(environ, start_response, app_config, location):
+    if environ and app_config:
         pass  # Lets ignore not usage PyCharm
     start_response('301 Moved Permanently', [('Location', location)])
     return b''
